2185427882e00c7b2febf6149bd4bb086cb63501
Omar Abuhadid <omrs2002@yahoo.com>
2022-08-16 14:40:07 +0300

    Add Builder Design patterns

---
+++ ConsoleDesignPatterns.Builder/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleDesignPatterns.Builder
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+
+}
